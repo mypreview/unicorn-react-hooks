9b18c228da279b0bd05d76ae78b1f440f56519e9
Mahdi Yazdani <mahdiyazdani@msn.com>
2022-05-02 23:15:02 +0300

    Retrieve the block support value for the "Layout"

---
+++ src/useGetBlockLayout/index.js
@@ -0,0 +1,32 @@
+/**
+ * Retrieve the block support value for a feature, if defined.
+ *
+ * @ignore
+ */
+import { getBlockSupport } from '@wordpress/blocks';
+
+/**
+ * WordPress specific abstraction layer atop React.
+ *
+ * @ignore
+ */
+import { useMemo } from '@wordpress/element';
+
+/**
+ * Retrieves the block support value for the "Layout" settings to be used in the preview.
+ *
+ * @function
+ * @since       1.0.0
+ * @name 		useGetBlockLayout
+ * @param       {string}    blockTypeOrName    The name for a block is a unique string that identifies a block.
+ * @return      {Object} 			           Block level "Layout" settings object.
+ * @example
+ *
+ * const { default } = useGetBlockLayout( 'core/social-links' );
+ *
+ * // { 'type': 'flex' }
+ */
+export default ( blockTypeOrName ) => {
+	const layoutBlockSupportConfig = useMemo( () => getBlockSupport( blockTypeOrName, '__experimentalLayout' ) || {}, [ blockTypeOrName ] );
+	return layoutBlockSupportConfig;
+};
