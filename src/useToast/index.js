b7b88d2de350642a2b25afd13cc67d14527a796e
Mahdi Yazdani <mahdiyazdani@msn.com>
2022-05-02 23:31:21 +0300

    Generates custom (notification) toast

---
+++ src/useToast/index.js
@@ -0,0 +1,25 @@
+/**
+ * Data module to manage application state for both plugins and WordPress itself.
+ * The data module is built upon and shares many of the same core principles of Redux.
+ *
+ * @ignore
+ */
+import { useDispatch } from '@wordpress/data';
+
+/**
+ * Generates custom (notification) toast messages.
+ *
+ * @function
+ * @since       1.0.0
+ * @return      {Function}    A function that could be called to initiate toast messages.
+ * @example
+ *
+ * const toast = useToast();
+ * toast( 'Text to display as a toast message!' );
+ */
+export default () => {
+	const { createNotice } = useDispatch( 'core/notices' );
+	const toast = ( text, args, type = 'error' ) => createNotice( type, text, { isDismissible: true, type: 'snackbar', ...args } );
+
+	return toast;
+};
