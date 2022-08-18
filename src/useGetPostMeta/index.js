d24ee85bcddababbb189e569653d4fd3be375b0e
Mahdi Yazdani <mahdiyazdani@msn.com>
2022-08-18 15:49:04 +0300

    Export post-type label and key

--- src/useGetPostMeta/index.js
+++ src/useGetPostMeta/index.js
@@ -16,24 +16,25 @@ import { useSelect } from '@wordpress/data';
  * Retrieves a post meta field of the current post.
  *
  * @function
- * @since       1.0.0
+ * @since       1.1.0
  * @name 		useGetPostMeta
  * @param       {string}    metaKey    The meta key to retrieve.
  * @return      {Object}			   An object containing currently viewed post-id and meta field value.
  * @example
  *
- * const { metaValue, postId, postSlug } = useGetPostMeta( 'prefix_blocks_theme_meta' );
+ * const { metaValue, postId, postSlug, postType } = useGetPostMeta( 'meta_key' );
  */
 export default ( metaKey ) => {
-	const { postId, postMeta, postSlug } = useSelect( ( select ) => {
-		const { getCurrentPostId, getEditedPostAttribute } = select( 'core/editor' );
+	const { postId, postMeta, postSlug, postType } = useSelect( ( select ) => {
+		const { getCurrentPostId, getCurrentPostType, getEditedPostAttribute, getPostTypeLabel } = select( 'core/editor' );
 
 		return {
 			postId: getCurrentPostId(),
 			postMeta: getEditedPostAttribute( 'meta' ),
 			postSlug: getEditedPostAttribute( 'slug' ),
+			postType: { key: getCurrentPostType(), label: getPostTypeLabel() },
 		};
 	} );
 
-	return { metaValue: get( postMeta, metaKey ), postId, postSlug };
+	return { metaValue: get( postMeta, metaKey ), postId, postSlug, postType };
 };
