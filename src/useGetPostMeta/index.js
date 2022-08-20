b49bddd496f31296144ca1a8ccbf3174a556d9d2
Mahdi Yazdani <mahdiyazdani@msn.com>
2022-08-20 19:33:52 +0300

    Include post title in the exported object

--- src/useGetPostMeta/index.js
+++ src/useGetPostMeta/index.js
@@ -16,25 +16,26 @@ import { useSelect } from '@wordpress/data';
  * Retrieves a post meta field of the current post.
  *
  * @function
- * @since       1.1.0
+ * @since       1.2.0
  * @name 		useGetPostMeta
  * @param       {string}    metaKey    The meta key to retrieve.
  * @return      {Object}			   An object containing currently viewed post-id and meta field value.
  * @example
  *
- * const { metaValue, postId, postSlug, postType } = useGetPostMeta( 'meta_key' );
+ * const { metaValue, postId, postType } = useGetPostMeta( 'meta_key' );
  */
 export default ( metaKey ) => {
-	const { postId, postMeta, postSlug, postType } = useSelect( ( select ) => {
+	const { postId, postMeta, postSlug, postTitle, postType } = useSelect( ( select ) => {
 		const { getCurrentPostId, getCurrentPostType, getEditedPostAttribute, getPostTypeLabel } = select( 'core/editor' );
 
 		return {
 			postId: getCurrentPostId(),
 			postMeta: getEditedPostAttribute( 'meta' ),
 			postSlug: getEditedPostAttribute( 'slug' ),
+			postTitle: getEditedPostAttribute( 'title' ),
 			postType: { key: getCurrentPostType(), label: getPostTypeLabel() },
 		};
 	} );
 
-	return { metaValue: get( postMeta, metaKey ), postId, postSlug, postType };
+	return { metaValue: get( postMeta, metaKey ) || postMeta, postId, postSlug, postTitle, postType };
 };
